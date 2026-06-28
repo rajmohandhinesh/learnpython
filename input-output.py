a3255e2d53d6462ef25e2adae7fbd4ab2caae56d
rajmohandhinesh <rajmohandhinesh@gmail.com>
2026-06-28 10:35:23 -0700

    Create input-output.py

---
+++ input-output.py
@@ -0,0 +1,22 @@
+name = input("Enter your name: ")
+print("Hello, " + name + "!")
+
+age = input(" Enter your age: ")
+print("You are " + age + " years old.")
+
+print("Hello, " + name + "! You are " + age + " years old.")
+
+name = "Dhinesh"
+age = 16
+print(name)
+print(age)
+
+score = int(input("Enter your score: "))
+if score >= 90:
+    print("You got an A!")
+elif score >= 80:
+    print("You got a B!")
+elif score >= 70:
+    print("You got a C!")
+else:
+    print("You got an F!")

31b72c971410486d3baa72e00532d525283f0d82
rajmohandhinesh <rajmohandhinesh@gmail.com>
2026-06-28 10:58:08 -0700

    Update input-output.py

--- input-output.py
+++ input-output.py
@@ -1,22 +1,33 @@
-name = input("Enter your name: ")
-print("Hello, " + name + "!")
+# name = input("Enter your name: ")
+# print("Hello, " + name + "!")
 
-age = input(" Enter your age: ")
-print("You are " + age + " years old.")
+# age = input(" Enter your age: ")
+# print("You are " + age + " years old.")
 
-print("Hello, " + name + "! You are " + age + " years old.")
+# print("Hello, " + name + "! You are " + age + " years old.")
 
-name = "Dhinesh"
-age = 16
-print(name)
-print(age)
+# name = "Dhinesh"
+# age = 16
+# print(name)
+# print(age)
 
-score = int(input("Enter your score: "))
-if score >= 90:
-    print("You got an A!")
-elif score >= 80:
-    print("You got a B!")
-elif score >= 70:
-    print("You got a C!")
+# score = int(input("Enter your score: "))
+# if score >= 90:
+#     print("You got an A!")
+# elif score >= 80:
+#     print("You got a B!")
+# elif score >= 70:
+#     print("You got a C!")
+# else:
+#     print("You got an F!")
+
+color = input("Enter your favorite color: ")
+if color == "red":
+    print("Red is a warm color.")
+elif color == "blue":
+    print("Blue is a cool color.")
+elif color != "green":
+    print("The color is not red, blue, or green. It is: " + color)
 else:
-    print("You got an F!")
+    print("I don't know what color that is.")
+
